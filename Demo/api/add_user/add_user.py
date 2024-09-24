3c95c838501258eaa634c98822b330a9bdc51a66
Casper Kristiansson <casper.kristiansson@yahoo.se>
2024-09-24 19:49:40 +0200

    Add user improve return message structure

--- Demo/api/add_user/add_user.py
+++ Demo/api/add_user/add_user.py
@@ -28,7 +28,9 @@ def lambda_handler(event, context):
 
         return {
             'statusCode': 200,
-            'body': json.dumps('Item inserted successfully'),
+            'body': json.dumps({
+                'message': 'User added successfully',
+            }),
             'headers': {
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Headers': 'Content-Type',
@@ -41,7 +43,9 @@ def lambda_handler(event, context):
     except Exception as e:
         return {
             'statusCode': 500,
-            'body': json.dumps(f'Error inserting item: {str(e)}'),
+            'body': json.dumps({
+                'message': f'Error inserting item: {str(e)}',
+            }),
             'headers': {
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Headers': 'Content-Type',
